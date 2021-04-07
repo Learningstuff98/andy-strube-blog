e1907e97c000d085a9f2ab62a7c9bcb882a45630
Andy Strube <andystrube64@gmail.com>
2021-04-06 18:14:38 -0700

    Posts can be updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,15 +5,29 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
-    redirect_to post_path(@post)
+    post = Post.create(post_params)
+    redirect_to post_path(post)
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = current_post
+  end
+
+  def edit
+    @post = current_post
+  end
+
+  def update
+    post = current_post
+    post.update(post_params)
+    redirect_to post_path(post)
   end
 
   private
+
+  def current_post
+    @post ||= Post.find(params[:id])
+  end
 
   def post_params
     params.require(:post).permit(:title, :content)
